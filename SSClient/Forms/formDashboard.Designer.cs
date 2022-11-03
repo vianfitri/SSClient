137db7ce8f451a19e95322fd76dce8b455ff6fa4
vianfitri <viancukiep@gmail.com>
2022-11-04 05:31:23 +0700

    commit

--- SSClient/Forms/formDashboard.Designer.cs
+++ SSClient/Forms/formDashboard.Designer.cs
@@ -36,14 +36,14 @@ namespace SSClient
             this.txtTimeE = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.hiddenPannel = new System.Windows.Forms.Panel();
-            this.btnSettings = new FontAwesome.Sharp.IconButton();
-            this.btnStability = new FontAwesome.Sharp.IconButton();
             this.pnlUserPic = new System.Windows.Forms.Panel();
-            this.btnLogout = new ViControls.ViButton();
             this.lblPriviledge = new System.Windows.Forms.Label();
             this.lblUsername = new System.Windows.Forms.Label();
-            this.userPic = new ViControls.CircularPicture();
             this.pnlContent = new System.Windows.Forms.Panel();
+            this.btnSettings = new FontAwesome.Sharp.IconButton();
+            this.btnStability = new FontAwesome.Sharp.IconButton();
+            this.btnLogout = new ViControls.ViButton();
+            this.userPic = new ViControls.CircularPicture();
             this.pnlMainMenu.SuspendLayout();
             this.panel1.SuspendLayout();
             this.pnlUserPic.SuspendLayout();
@@ -140,6 +140,53 @@ namespace SSClient
             this.hiddenPannel.Size = new System.Drawing.Size(250, 18);
             this.hiddenPannel.TabIndex = 23;
             // 
+            // pnlUserPic
+            // 
+            this.pnlUserPic.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
+            this.pnlUserPic.Controls.Add(this.btnLogout);
+            this.pnlUserPic.Controls.Add(this.lblPriviledge);
+            this.pnlUserPic.Controls.Add(this.lblUsername);
+            this.pnlUserPic.Controls.Add(this.userPic);
+            this.pnlUserPic.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlUserPic.Location = new System.Drawing.Point(0, 0);
+            this.pnlUserPic.Margin = new System.Windows.Forms.Padding(2);
+            this.pnlUserPic.Name = "pnlUserPic";
+            this.pnlUserPic.Size = new System.Drawing.Size(250, 80);
+            this.pnlUserPic.TabIndex = 0;
+            // 
+            // lblPriviledge
+            // 
+            this.lblPriviledge.AutoSize = true;
+            this.lblPriviledge.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPriviledge.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(139)))), ((int)(((byte)(144)))));
+            this.lblPriviledge.Location = new System.Drawing.Point(76, 39);
+            this.lblPriviledge.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPriviledge.Name = "lblPriviledge";
+            this.lblPriviledge.Size = new System.Drawing.Size(76, 15);
+            this.lblPriviledge.TabIndex = 3;
+            this.lblPriviledge.Text = "super admin";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.ForeColor = System.Drawing.Color.White;
+            this.lblUsername.Location = new System.Drawing.Point(76, 20);
+            this.lblUsername.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(88, 20);
+            this.lblUsername.TabIndex = 2;
+            this.lblUsername.Text = "username";
+            // 
+            // pnlContent
+            // 
+            this.pnlContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlContent.Location = new System.Drawing.Point(250, 0);
+            this.pnlContent.Margin = new System.Windows.Forms.Padding(2);
+            this.pnlContent.Name = "pnlContent";
+            this.pnlContent.Size = new System.Drawing.Size(1334, 821);
+            this.pnlContent.TabIndex = 1;
+            // 
             // btnSettings
             // 
             this.btnSettings.Dock = System.Windows.Forms.DockStyle.Top;
@@ -192,26 +239,12 @@ namespace SSClient
             this.btnStability.Visible = false;
             this.btnStability.Click += new System.EventHandler(this.btnStability_Click);
             // 
-            // pnlUserPic
-            // 
-            this.pnlUserPic.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
-            this.pnlUserPic.Controls.Add(this.btnLogout);
-            this.pnlUserPic.Controls.Add(this.lblPriviledge);
-            this.pnlUserPic.Controls.Add(this.lblUsername);
-            this.pnlUserPic.Controls.Add(this.userPic);
-            this.pnlUserPic.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlUserPic.Location = new System.Drawing.Point(0, 0);
-            this.pnlUserPic.Margin = new System.Windows.Forms.Padding(2);
-            this.pnlUserPic.Name = "pnlUserPic";
-            this.pnlUserPic.Size = new System.Drawing.Size(250, 80);
-            this.pnlUserPic.TabIndex = 0;
-            // 
             // btnLogout
             // 
             this.btnLogout.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnLogout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
             this.btnLogout.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
-            this.btnLogout.BackgroundImage = global::SSInstructor.Properties.Resources.logout2;
+            this.btnLogout.BackgroundImage = global::SSClient.Properties.Resources.logout3;
             this.btnLogout.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnLogout.BorderColor = System.Drawing.Color.PaleVioletRed;
             this.btnLogout.BorderRadius = 28;
@@ -228,40 +261,16 @@ namespace SSClient
             this.btnLogout.UseVisualStyleBackColor = false;
             this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
             // 
-            // lblPriviledge
-            // 
-            this.lblPriviledge.AutoSize = true;
-            this.lblPriviledge.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPriviledge.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(139)))), ((int)(((byte)(144)))));
-            this.lblPriviledge.Location = new System.Drawing.Point(76, 39);
-            this.lblPriviledge.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblPriviledge.Name = "lblPriviledge";
-            this.lblPriviledge.Size = new System.Drawing.Size(76, 15);
-            this.lblPriviledge.TabIndex = 3;
-            this.lblPriviledge.Text = "super admin";
-            // 
-            // lblUsername
-            // 
-            this.lblUsername.AutoSize = true;
-            this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUsername.ForeColor = System.Drawing.Color.White;
-            this.lblUsername.Location = new System.Drawing.Point(76, 20);
-            this.lblUsername.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(88, 20);
-            this.lblUsername.TabIndex = 2;
-            this.lblUsername.Text = "username";
-            // 
             // userPic
             // 
-            this.userPic.BackgroundImage = global::SSInstructor.Properties.Resources.user1;
+            this.userPic.BackgroundImage = global::SSClient.Properties.Resources.user2;
             this.userPic.BorderCapStyle = System.Drawing.Drawing2D.DashCap.Flat;
             this.userPic.BorderColor = System.Drawing.Color.RoyalBlue;
             this.userPic.BorderColor2 = System.Drawing.Color.HotPink;
             this.userPic.BorderLineStyle = System.Drawing.Drawing2D.DashStyle.Solid;
             this.userPic.BorderSize = 1;
             this.userPic.GradientAngle = 50F;
-            this.userPic.Image = global::SSInstructor.Properties.Resources.user1;
+            this.userPic.Image = global::SSClient.Properties.Resources.user2;
             this.userPic.Location = new System.Drawing.Point(8, 9);
             this.userPic.Margin = new System.Windows.Forms.Padding(2);
             this.userPic.Name = "userPic";
@@ -269,15 +278,6 @@ namespace SSClient
             this.userPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.userPic.TabIndex = 1;
             this.userPic.TabStop = false;
-            // 
-            // pnlContent
-            // 
-            this.pnlContent.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlContent.Location = new System.Drawing.Point(250, 0);
-            this.pnlContent.Margin = new System.Windows.Forms.Padding(2);
-            this.pnlContent.Name = "pnlContent";
-            this.pnlContent.Size = new System.Drawing.Size(1334, 821);
-            this.pnlContent.TabIndex = 1;
             // 
             // formDashboard
             // 
--- SSClient/Properties/Resources.Designer.cs
+++ SSClient/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SSInstructor.Properties {
+namespace SSClient.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SSInstructor.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SSInstructor.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SSClient.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -136,6 +136,16 @@ namespace SSInstructor.Properties {
         internal static System.Drawing.Bitmap logout2 {
             get {
                 object obj = ResourceManager.GetObject("logout2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap logout3 {
+            get {
+                object obj = ResourceManager.GetObject("logout3", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
@@ -266,6 +276,16 @@ namespace SSInstructor.Properties {
         internal static System.Drawing.Bitmap user1 {
             get {
                 object obj = ResourceManager.GetObject("user1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap user2 {
+            get {
+                object obj = ResourceManager.GetObject("user2", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
